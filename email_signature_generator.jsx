8146f532ea65bf4a49ebf51cfaab10854ab6b809
AkhilMangalan <67104290+AkhilMangalan@users.noreply.github.com>
2026-01-31 23:43:50 +0530

    Update email_signature_generator.jsx
    Signed-off-by: AkhilMangalan <67104290+AkhilMangalan@users.noreply.github.com>

--- email_signature_generator.jsx
+++ email_signature_generator.jsx
@@ -42,7 +42,7 @@ function App() {
   
   <div style={{ display: "flex", alignItems: "center", gap: "12px", marginBottom: "16px" }}>
   <img
-    src="https://github.com/AkhilMangalan/Image/blob/main/mnemonic_logo.png"
+    src="https://raw.githubusercontent.com/AkhilMangalan/Image/refs/heads/main/hero.png"
     alt="RAMP360"
     style={{ height: "48px" }}
   />
